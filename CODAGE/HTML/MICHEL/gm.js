bd11a961f0b5e9dbd5ddd312365e95dbeaec8bfa
Alex1blg <alexisbeligne@gmail.com>
2023-09-30 22:41:12 +0200

    first commit

--- CODAGE/HTML/MICHEL/gm.js
+++
@@ -1,7 +0,0 @@
-const menuHamburger = document.querySelector(".navbar .menu-hamburger")
-const navLinks = document.querySelector(".navbar .nav-links")
-
-menuHamburger.addEventListener('click',()=>{
-navLinks.classList.toggle('mobile-menu')
-})
-
--- CODAGE/HTML/MICHEL/gm2.js
+++
@@ -1,6 +0,0 @@
-const imgPetite = document.querySelector(".panier .img-petite .img-petite-child1")
-const imgGrande = document.querySelector(".panier .img-grande .img-grande-child1")
-
-imgPetite.addEventListener('click',()=>{
-imgGrande.classList.toggle('mobile-menu')
-})  
